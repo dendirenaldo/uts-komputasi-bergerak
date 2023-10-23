8ef74a61846d8227418d04203d66c5eeacf535e2
Dendi Renaldo Permana <dendi.renaldo@gmail.com>
2023-10-23 14:51:22 +0700

    Init App

--- App.js
+++ App.js
@@ -1,23 +1,132 @@
+import { AntDesign } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import React from 'react';
-import { AppProvider } from './context/AppProvider';
-import Route from './Route';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Dimensions, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Data from './Data';
 
 export default function App() {
+  const width = Dimensions.get('window').width;
   return (
     <React.Fragment>
-      <AppProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <SafeAreaView style={{ flex: 1 }}>
-            <Route />
-            <StatusBar style='auto' />
-          </SafeAreaView>
-        </GestureHandlerRootView>
-      </AppProvider>
+      <ScrollView style={{ flexGrow: 1 }}>
+        <View style={[styles.container, { width: width, height: (width * 3) / 2 }]}>
+          <Image style={[styles.gambarBG, { width: width, height: (width * 3) / 2 }]} source={Data[0].gambar} />
+          <View style={styles.header}>
+            <Text style={styles.headerTeks}>{Data[0].institusi}</Text>
+          </View>
+          <View style={styles.deskripsi}>
+            <View style={styles.lokasi}>
+              <Text style={[styles.lokasiTeks, { maxWidth: width * 0.7 }]}>{Data[0].nama}</Text>
+              <View style={styles.rating}>
+                {[1, 2, 3, 4, 5].map((item) => (
+                  <>
+                    <AntDesign name={item <= Data[0].rating ? 'heart' : 'hearto'} color="white" />
+                  </>
+                ))}
+              </View>
+            </View>
+            <View style={styles.separator}></View>
+            <Text style={styles.keterangan}>{Data[0].deskripsi}</Text>
+            <TouchableOpacity style={styles.tombol}>
+              <Text style={styles.tombolTeks}>LEBIH BANYAK&gt;</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ paddingHorizontal: 10, paddingVertical: 20 }}>
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 5 }}>Fakultas Lainnya</Text>
+          {Data.map((item, index) => {
+            if (index !== 0) {
+              return (
+                <View activeOpacity={0.7} style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 13, overFlow: 'hidden', width: ((width - 20) * 0.8) - 10 }}>
+                  <>
+                    <Image style={{ width: (width - 20) * 0.20, height: (width - 20) * 0.20, borderRadius: 12, marginRight: 10 }} source={item.gambar} />
+                    <View style={{ flexDirection: 'column' }}>
+                      <Text style={{ marginBottom: 5, fontSize: 15, fontWeight: 'bold' }}>{item.nama}</Text>
+                      <Text numberOfLines={2}>{item.institusi}</Text>
+                    </View>
+                  </>
+                </View>
+              )
+            }
+          })}
+        </View>
+      </ScrollView>
+      <StatusBar style="auto" />
     </React.Fragment>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    position: 'relative',
+    overflow: 'hidden'
+  },
+  deskripsi: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    paddingHorizontal: 15
+  },
+  lokasi: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  lokasiTeks: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  keterangan: {
+    color: 'white',
+    marginBottom: 10
+  },
+  separator: {
+    borderBottomColor: '#fff',
+    borderBottomWidth: 2,
+    marginTop: 8,
+  },
+  header: {
+    position: 'absolute',
+    top: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 15,
+    width: '100%'
+  },
+  headerTeks: {
+    color: 'white',
+    letterSpacing: 5,
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    fontWeight: 500
+  },
+  gambarBG: {
+    width: '100%',
+    paddingBottom: 10,
+    position: 'relative'
+  },
+  tombol: {
+    marginBottom: 10,
+    marginLeft: 200,
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    alignSelf: 'flex-end',
+    borderRadius: 100
+  },
+  tombolTeks: {
+    color: '#fff'
+  },
+  rating: {
+    flexDirection: 'row',
+    gap: 4,
+  }
+});
 
--- Data.js
+++ Data.js
@@ -1,230 +1,32 @@
 const Data = [
     {
-        index: "fasilkom",
-        nama: "Fakultas Ilmu Komputer",
-        data: [
-            {
-                "gambar": require("./assets/1.jpg"),
-                "nama": "Fakultas Ilmu Komputer",
-                "institusi": "Universitas Sriwijaya",
-                "deskripsi": "Fakultas Ilmu Komputer Universitas Sriwijaya merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/2.jpg"),
-                "nama": "Fakultas Ilmu Komputer",
-                "institusi": "Universitas Indonesia",
-                "deskripsi": "Fakultas Ilmu Komputer Universitas Indonesia merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/4.png"),
-                "nama": "Fakultas Ilmu Komputer",
-                "institusi": "Universitas Gadjah Mada",
-                "deskripsi": "Fakultas Ilmu Komputer Universitas Riau merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/5.jpg"),
-                "nama": "Sekolah Tinggi Elektro dan Informatika",
-                "institusi": "Institut Teknologi Bandung",
-                "deskripsi": "Sekolah Tinggi Elektro dan Informatika Institut Teknologi Bandung merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            }
-        ],
-    }, {
-        index: "ft",
-        nama: "Fakultas Teknik",
-        data: [
-            {
-                "gambar": require("./assets/1.jpg"),
-                "nama": "Fakultas Teknik",
-                "institusi": "Universitas Sriwijaya",
-                "deskripsi": "Fakultas Teknik Universitas Sriwijaya merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/2.jpg"),
-                "nama": "Fakultas Teknik",
-                "institusi": "Universitas Indonesia",
-                "deskripsi": "Fakultas Teknik Universitas Indonesia merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/3.jpg"),
-                "nama": "Fakultas Teknik",
-                "institusi": "Universitas Riau",
-                "deskripsi": "Fakultas Teknik Universitas Riau merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/4.png"),
-                "nama": "Fakultas Teknik",
-                "institusi": "Universitas Gadjah Mada",
-                "deskripsi": "Fakultas Teknik Universitas Riau merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/5.jpg"),
-                "nama": "Sekolah Tinggi Elektro dan Informatika",
-                "institusi": "Institut Teknologi Bandung",
-                "deskripsi": "Sekolah Tinggi Elektro dan Informatika Institut Teknologi Bandung merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            }
-        ],
-    }, {
-        index: "fmipa",
-        nama: "Fakultas Matematika dan Pengetahuan Alam",
-        data: [
-            {
-                "gambar": require("./assets/1.jpg"),
-                "nama": "Fakultas Matematika dan Pengetahuan Alam",
-                "institusi": "Universitas Sriwijaya",
-                "deskripsi": "Fakultas Matematika dan Pengetahuan Alam Universitas Sriwijaya merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/2.jpg"),
-                "nama": "Fakultas Matematika dan Pengetahuan Alam",
-                "institusi": "Universitas Indonesia",
-                "deskripsi": "Fakultas Matematika dan Pengetahuan Alam Universitas Indonesia merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/3.jpg"),
-                "nama": "Fakultas Matematika dan Pengetahuan Alam",
-                "institusi": "Universitas Riau",
-                "deskripsi": "Fakultas Matematika dan Pengetahuan Alam Universitas Riau merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/4.png"),
-                "nama": "Fakultas Matematika dan Pengetahuan Alam",
-                "institusi": "Universitas Gadjah Mada",
-                "deskripsi": "Fakultas Matematika dan Pengetahuan Alam Universitas Riau merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            },
-            {
-                "gambar": require("./assets/5.jpg"),
-                "nama": "Fakultas Matematika dan Pengetahuan Alam",
-                "institusi": "Institut Teknologi Bandung",
-                "deskripsi": "Fakultas Matematika dan Pengetahuan Alam Institut Teknologi Bandung merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
-                "rating": [{
-                    value: 4,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }, {
-                    value: 5,
-                    createdAt: "2023-03-01T00:00:00.000Z",
-                    isMe: false
-                }]
-            }
-        ],
-    }
+        "gambar": require("./assets/1.jpg"),
+        "nama": "Fakultas Ilmu Komputer",
+        "institusi": "Universitas Sriwijaya",
+        "deskripsi": "Fakultas Ilmu Komputer Universitas Sriwijaya merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
+        "rating": 4.5
+    },
+    {
+        "gambar": require("./assets/2.jpg"),
+        "nama": "Fakultas Teknik",
+        "institusi": "Universitas Sriwijaya",
+        "deskripsi": "Fakultas Teknik Universitas Sriwijaya merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
+        "rating": 3
+    },
+    {
+        "gambar": require("./assets/3.jpg"),
+        "nama": "Fakultas Matematika dan Ilmu Pengetahuan",
+        "institusi": "Universitas Sriwijaya",
+        "deskripsi": "Fakultas Matematika dan Ilmu Pengetahuan Universitas Sriwijaya merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
+        "rating": 3
+    },
+    {
+        "gambar": require("./assets/4.jpg"),
+        "nama": "Fakultas Ekonomi",
+        "institusi": "Universitas Sriwijaya",
+        "deskripsi": "Fakultas Ekonomi Universitas Sriwijaya merupakan Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem exercitationem eligendi iusto rerum. Placeat similique accusamus accusantium officiis dolorum, est officia incidunt, aliquid vitae omnis eos, dolor eligendi delectus dignissimos.",
+        "rating": 4
+    },
 ]
 
 export default Data;
